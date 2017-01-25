01cdecc7253aba3499515c27645613e0754cb7e8
fattombo <fattombo@gmail.com>
2017-01-25 16:02:59 +0100

    added instagram feed loading

--- 2_dev/app/js/app.js
+++ 2_dev/app/js/app.js
@@ -1,5 +1,28 @@
 smoothScroll.init();
 
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    req.send();
+  });
+}
+
+
 Vue.component('project', {
   props: ['url', 'title'],
   template: '<h3><a :href="url">{{ title }}</a></h3>'
@@ -12,3 +35,26 @@ var app = new Vue({
     message: 'Vue test!'
   }
 })
+
+
+var feed = new Instafeed({
+  get: 'user',
+  userId: 'self',
+  accessToken: '1497992223.54da896.46ee4bcf6aa148b78d4b2899b6a2eca0',
+  target: 'insta',
+  resolution: 'standard_resolution',
+  limit: 3,
+  template: '<div class="insta__pic"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></div>',
+  success: function() {
+    document.querySelector('.insta__load').style.display = 'none';
+  },
+  error: function() {
+    var el = document.querySelector('.insta__load');
+    if (el.textContent) {
+      el.textContent = 'oh my, it takes a little too long! probably an error has occured while loading instagram feed';
+    } else {
+      el.innerText = 'oh my, it takes a little too long! probably an error has occured while loading instagram feed';
+    }
+  }
+});
+feed.run();
